8992e6c57dbfac50330fe1faae636930526016ce
LawWhite <58451098+LawWhite@users.noreply.github.com>
2019-12-07 12:41:11 -0700

    Create LPHashTable.hpp

---
+++ LPHashTable.hpp
@@ -0,0 +1,29 @@
+
+#ifndef LPHASHTABLE_H
+#define LPHASHTABLE_H
+
+#include <iostream>
+
+using namespace std;
+
+
+class LPHashTable{
+  private:
+      int TABLE_SIZE;
+      int *LPHashTable;
+      bool functnOne;
+      int elements;
+      float loadFactor;
+  public:
+      LPHashTable(int tableSize, bool hashFunction);
+      ~LPHashTable();
+      int hashFunctOne(int key);
+      int hashFunctTwo(int key);
+      void printTable();
+      void insertKey(int key);
+      int searchKey(int key);
+      void deleteK(int key);
+      float getLoadFactor();
+};
+
+#endif
