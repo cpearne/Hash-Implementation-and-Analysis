3d3ab7eff0a626d89182093f3f9a4eb33eab8eee
cpearne <58447865+cpearne@users.noreply.github.com>
2019-12-08 15:15:14 -0700

    Update outputs.cpp

--- outputs.cpp
+++ outputs.cpp
@@ -1,1 +1,112 @@
+DATA SET A:
+  LINKED LIST:
+    Insert Data: 1.9e-05, 1.9e-05, 4.1e-05, 2e-05, 2e-05, 1.8e-05
+    Search Data: 6e-06, 5e-06, 1.3e-05, 5e-06, 5e-06, 2.8e-05
+    Delete Data: 2.8e-05, 2.6e-05, 4.3e-05, 2.6e-05, 2.7e-05, 2.2e-05
 
+    Insert Data: 1.4e-05, 1.7e-05, 1.3e-05, 1.4e-05, 1.5e-05, 1.5e-05
+    Search Data: 3e-06, 4e-06, 2e-06, 4e-06, 3e-06, 3e-06
+    Delete Data: 1.9e-05, 1.8e-05, 1.5e-05, 1.5e-05, 1.6e-05, 1.7e-05
+
+    Insert Data: 1.5e-05, 1.6e-05, 1.4e-05, 1.5e-05, 1.5e-05, 1.7e-05
+    Search Data: 3e-06, 3e-06, 4e-06, 4e-06, 4e-06, 5e-06
+    Delete Data: 1.9e-05, 1.9e-05, 1.6e-05, 1.7e-05, 1.9e-05, 1.9e-05
+      
+ BINARY SEARCH TREE:
+    Insert Data: 1.5e-05, 1.6e-05, 1.7e-05, 2.3e-05, 2.1e-05, 2.4e-05
+    Search Data: 3e-06, 2e-06, 3e-06, 3e-06, 5e-06, 6e-06
+    Delete Data: 1.8e-05, 1.6e-05, 1.4e-05, 1.9e-05, 1.7e-05, 1.7e-05
+    
+    Insert Data: 1.6e-05, 2e-05, 2e-05, 1.9e-05, 2e-05, 2.5e-05
+    Search Data: 5e-06, 4e-06, 4e-06, 4e-06, 4e-06, 5e-06
+    Delete Data: 1.8e-05, 1.8e-05, 1.5e-05, 1.3e-05, 1.5e-05, 1.8e-05
+      
+    Insert Data: 1.7e-05, 2.1e-05, 2.1e-05, 2.4e-05, 2.4e-05, 5.7e-05
+    Search Data: 4e-06, 4e-06, 3e-06, 6e-06, 6e-06, 1.9e-05
+    Delete Data: 2e-05, 1.8e-05, 1.6e-05, 1.8e-05, 1.9e-05, 6.4e-05
+      
+ LINEAR PROBING:
+    Insert Data: 0.001979, 0.001969, 0.002001, 0.001968, 0.001988, 0.001982
+    Search Data: 1e-06, 1e-06, 2e-06, 3e-06, 5e-06, 7e-06
+    Delete Data: 2.3e-05, 2.4e-05, 2.6e-05, 2.6e-05, 2.5e-05, 3.3e-05
+      
+    Insert Data: 0.001969, 0.001983, 0.001968, 0.001987, 0.002016, 0.002031
+    Search Data: 2e-06, 2e-06, 2e-06, 2e-06, 4e-06, 1.4e-05
+    Delete Data: 2.3e-05, 2.4e-05, 2.4e-05, 2.6e-05, 2.5e-05, 2.7e-05
+      
+    Insert Data: 0.001974, 0.002142, 0.00198, 0.001974, 0.001975, 0.001986
+    Search Data: 2e-06, 2e-06, 4e-06, 4e-06, 5e-06, 8e-06
+    Delete Data: 2.4e-05, 2.4e-05, 2.6e-05, 2.6e-05, 2.7e-05, 3.9e-05
+      
+ CUCKOO HASHING:
+    Insert Data: 4e-06, 4e-06, 4e-06, 5e-06, 4e-06, 3e-06
+    Search Data: 2e-06, 1e-06, 1e-06, 1e-06, 0, 0
+    Delete Data: 0.003323, 0.003166, 0.002621, 0.002543, 0.002532, 0.002205
+    Number of times Rehashed: 22
+      
+    Insert Data: 3e-06, 3e-06, 2e-06, 4e-06, 3e-06, 2e-06
+    Search Data: 2e-06, 1e-06, 1e-06, 1e-06, 2e-06, 0
+    Delete Data: 0.003339, 0.003149, 0.002603, 0.002543, 0.002541, 0.002221
+    Number of times Rehashed: 22
+      
+    Insert Data: 4e-06, 4e-06, 3e-06, 3e-06, 3e-06, 8e-06
+    Search Data: 1e-06, 1e-06, 1e-06, 1e-06, 1e-06, 2e-06
+    Delete Data: 0.003325, 0.003147, 0.002624, 0.002542, 0.00252, 0.002287
+    Number of times Rehashed: 22
+      
+      
+DATA SET C:
+ LINKED LIST:
+    Insert Data: 1.3e-05, 1.5e-05, 1.3e-05, 1.6e-05, 1.4e-05, 1.5e-05
+    Search Data: 2e-06, 3e-06, 3e-06, 3e-06, 4e-06, 6e-06
+    Delete Data: 1.7e-05, 1.7e-05, 1.4e-05, 2.3e-05, 1.5e-05, 1.7e-05
+      
+    Insert Data: 1.3e-05, 1.5e-05, 1.4e-05, 1.4e-05, 1.3e-05, 1.6e-05
+    Search Data: 4e-06, 4e-06, 3e-06, 3e-06, 4e-06, 4e-06
+    Delete Data: 1.8e-05, 1.8e-05, 1.5e-05, 1.5e-05, 1.4e-05, 1.6e-05
+      
+    Insert Data: 1.5e-05, 1.9e-05, 1.6e-05, 1.7e-05, 1.8e-05, 1.9e-05
+    Search Data: 5e-06, 4e-06, 5e-06, 6e-06, 6e-06, 6e-06
+    Delete Data: 2e-05, 1.9e-05, 1.8e-05, 1.9e-05, 2e-05, 2.1e-05
+      
+ BINARY SEARCH TREE:
+    Insert Data: 1.5e-05, 2e-05, 2e-05, 2e-05, 2e-05, 2.3e-05
+    Search Data: 3e-06, 3e-06, 3e-06, 3e-06, 4e-06, 4e-06
+    Delete Data: 1.8e-05, 1.8e-05, 1.6e-05, 1.5e-05, 1.5e-05, 1.6e-05
+      
+    Insert Data: 1.8e-05, 1.3e-05, 1.7e-05, 4.9e-05, 2.6e-05, 1.6e-05
+    Search Data: 5e-06, 3e-06, 4e-06, 1.4e-05, 4e-06, 4e-06
+    Delete Data: 1.9e-05, 1.7e-05, 1.9e-05, 5.1e-05, 1.2e-05, 1.3e-05
+      
+    Insert Data: 2e-05, 2.4e-05, 2.5e-05, 2.6e-05, 3e-05, 2.8e-05
+    Search Data: 4e-06, 5e-06, 5e-06, 7e-06, 9e-06, 8e-06
+    Delete Data: 2e-05, 1.8e-05, 1.7e-05, 1.7e-05, 2.2e-05, 2.1e-05
+      
+ LINEAR PROBING:
+    Insert Data: 0.002093, 0.002112, 0.001979, 0.001972, 0.00202, 0.002022
+    Search Data: 2e-06, 1e-06, 2e-06, 3e-06, 5e-06, 6e-06
+    Delete Data: 2.3e-05, 2.4e-05, 2.5e-05, 2.5e-05, 2.7e-05, 3.8e-05
+      
+    Insert Data: 0.001984, 0.001973, 0.002006, 0.001982, 0.001976, 0.001976
+    Search Data: 1e-06, 2e-06, 2e-06, 2e-06, 5e-06, 2.5e-05
+    Delete Data: 2.3e-05, 2.3e-05, 2.5e-05, 2.6e-05, 2.7e-05, 3.3e-05
+      
+    Insert Data: 0.001972, 0.002052, 0.002031, 0.002078, 0.0022, 0.002178
+    Search Data: 3e-06, 3e-06, 4.4e-05, 0.000142, 0.000177, 0.000268
+    Delete Data: 2.3e-05, 2.5e-05, 8.7e-05, 0.000134, 0.000259, 0.000248
+
+ CUCKOO HASHING:
+    Insert Data: 3e-06, 4e-06, 3e-06, 3e-06, 1.9e-05, 7e-06
+    Search Data: 1e-06, 1e-06, 1e-06, 1e-06, 3e-06, 1e-06
+    Delete Data: 0.003285, 0.003164, 0.002669, 0.002541, 0.003164, 0.003074
+    Number of times Rehashed: 13
+      
+    Insert Data: 3e-06, 3e-06, 4e-06, 6e-06, 3e-06, 4e-06
+    Search Data: 1e-06, 2e-06, 1e-06, 1e-06, 1e-06, 2e-06
+    Delete Data: 0.003234, 0.003147, 0.002579, 0.002685, 0.002589, 0.002321
+    Number of times Rehashed: 14
+      
+    Insert Data: 3e-06, 4e-06, 3e-06, 3e-06, 4e-06, 4e-06
+    Search Data: 1e-06, 2e-06, 2e-06, 1e-06, 1e-06, 1e-06
+    Delete Data: 0.003338, 0.003171, 0.002631, 0.00254, 0.002541, 0.002207
+    Number of times Rehashed: 11
