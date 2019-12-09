47a99868727795b11531c101c92d4ca260c26ba2
cpearne <58447865+cpearne@users.noreply.github.com>
2019-12-08 20:58:51 -0700

    Update CHHashTable.cpp

--- CHHashTable.cpp
+++ CHHashTable.cpp
@@ -95,6 +95,49 @@ void CHHashTable::printTable()
   }
 }
 
+// Function that returns true if n
+// is prime else returns false
+bool isPrime(int n)
+{
+    // Corner cases
+    if (n <= 1)  return false;
+    if (n <= 3)  return true;
+
+    // This is checked so that we can skip
+    // middle five numbers in below loop
+    if (n%2 == 0 || n%3 == 0) return false;
+
+    for (int i=5; i*i<=n; i=i+6)
+        if (n%i == 0 || n%(i+2) == 0)
+           return false;
+
+    return true;
+}
+
+// Function to return the smallest
+// prime number greater than N
+int nextPrime(int N)
+{
+
+    // Base case
+    if (N <= 1)
+        return 2;
+
+    int prime = N;
+    bool found = false;
+
+    // Loop continuously until isPrime returns
+    // true for a number greater than n
+    while (!found) {
+        prime++;
+
+        if (isPrime(prime))
+            found = true;
+    }
+
+    return prime;
+}
+
 /*
  * Purpose: Function to create two new tables with a larger TABLE_SIZE
  */
@@ -103,7 +146,7 @@ void CHHashTable::reHash()
   //counter to get number of times reHashed
   rehash++;
   //incremente TABLE_SIZE by 1 and create two new tables with new tableSize
-  int newSize = TABLE_SIZE + 1;
+  int newSize = nextPrime(TABLE_SIZE);
   int *temp1 = new int [newSize];
   int *temp2 = new int [newSize];
   //set the indexes of the previous tables to the new ones
